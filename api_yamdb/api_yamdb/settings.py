d6de667855d16a6b91b469b3d8eb23723339c431
Иван Сиснёв <ivan.sisnev@gmail.com>
2023-01-25 13:46:44 +0300

    изменения settings.py (pytest ok)

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -70,11 +70,11 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE',
                             default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='db_name'),
-        'USER': os.getenv('POSTGRES_USER', default='postgres_user'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postres_password'),
-        'HOST': os.getenv('DB_HOST', default='db_host'),
-        'PORT': os.getenv('DB_PORT', default='db_port')
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
 
